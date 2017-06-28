d37d702e63c1dd0ee2151955ee8a5579afd4382b
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-06-27 21:57:15 -0400

    Add group generating, as inspired by gh user thomftz

---
+++ generator.rb
@@ -0,0 +1,19 @@
+#taught to me by @thomftz
+
+students = ['Parker', 'Grant', 'Robert', 'Enoc', 'Kelly', 'Georgia', 'Chris', 'Wyatt', 'Victoria', 'Ben', 'Brett', 'Sho', 'Helen', 'Jamie', 'Alex', 'Emma', 'Simran', 'Shontae', 'Seth', 'Ree', 'Kyle', 'Brandon']
+
+puts "How many students do you want grouped together?"
+group = gets.to_i
+
+students.shuffle.each_slice(group) {|person| puts person.join(" , ")}
+
+#shuffle - literally just shuffles: mixes up contents of whatever your tell it to
+  #i.e. a = [1,2,3,4]
+  #a.shuffle => [4,2,1,3]
+
+#each_slice - iterates through whatever you've specificed (in this case, students), to grab "slices" of however many you've specificed
+  #students.each_slice(2) will go through and grab groups of 2
+
+#|person| - declares new variable refering to the individual items within the array
+#p - reserved keyword that performs
+#person.join() - whatever is between the parentheses is concatonated between individual items
